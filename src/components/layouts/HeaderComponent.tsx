5bc9b25ca694f6c425d2944b2d81ae52fe86e124
yehezkielgunawan <yehezkiel.gunawan@student.umn.ac.id>
2022-04-14 20:55:54 +0700

    feat(headerComponent): :sparkles: add github repo link

--- src/components/layouts/HeaderComponent.tsx
+++ src/components/layouts/HeaderComponent.tsx
@@ -1,11 +1,12 @@
 import clsx from "clsx";
 import { useTheme } from "next-themes";
 import React, { useEffect, useState } from "react";
-import { FaMoon, FaSun } from "react-icons/fa";
+import { FaGithub, FaMoon, FaSun, FaWhatsapp } from "react-icons/fa";
 
 import clsxm from "@/lib/helpers/clsxm";
 
 import Button from "../buttons/Button";
+import ButtonLink from "../buttons/ButtonLink";
 
 const HeaderComponent = () => {
   const { theme, setTheme } = useTheme();
@@ -27,21 +28,34 @@ const HeaderComponent = () => {
           "mx-auto h-14 max-w-4xl px-2 md:px-1"
         )}
       >
-        <h5 className="font-bold text-black dark:text-white">
-          Whatsapp Helper
-        </h5>
-        <Button
-          variant="outline"
-          className={clsxm(
-            "dark:border-white dark:text-white dark:hover:bg-gray-500",
-            "hover:border-black hover:bg-gray-200",
-            "border-black p-2 text-black",
-            "rounded-full"
-          )}
-          onClick={() => handleChangeTheme()}
-        >
-          {theme === "dark" ? <FaSun size={20} /> : <FaMoon size={20} />}
-        </Button>
+        <div className="inline-flex items-center gap-1">
+          <FaWhatsapp size={24} />
+          <h5 className="font-bold text-black dark:text-white">
+            WhatsApp Helper
+          </h5>
+        </div>
+        <div className="inline-flex items-center gap-3">
+          <ButtonLink
+            variant="ghost"
+            href="https://github.com/yehezkielgunawan/wa-helper"
+            className="gap-2"
+          >
+            <FaGithub size={16} />
+            Github Repo
+          </ButtonLink>
+          <Button
+            variant="outline"
+            className={clsxm(
+              "dark:border-white dark:text-white dark:hover:bg-gray-500",
+              "hover:border-black hover:bg-gray-200",
+              "border-black p-2 text-black",
+              "rounded-full"
+            )}
+            onClick={() => handleChangeTheme()}
+          >
+            {theme === "dark" ? <FaSun size={20} /> : <FaMoon size={20} />}
+          </Button>
+        </div>
       </div>
     </header>
   );
