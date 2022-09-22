de242c5dbd7283366064b374c384c95842668bad
yehezkielgunawan <48091142+yehezkielgunawan@users.noreply.github.com>
2022-09-22 16:31:45 +0700

    Update src/__test__/index.test.tsx
    Co-authored-by: Nicholas <31909304+lauslim12@users.noreply.github.com>

--- src/__test__/index.test.tsx
+++ src/__test__/index.test.tsx
@@ -5,8 +5,8 @@ import { render } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import Home from "@/pages/index";
 
-describe("Render Main Page Successfully", () => {
-  it("Renders Home Page Successfully", () => {
+describe("<Home />", () => {
+  it("renders home page successfully", () => {
     render(<Home countryCodes={[]} />);
   });
 });
