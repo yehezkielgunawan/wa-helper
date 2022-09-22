127ebc8e9a84667634a7804f42a91ffc224967cb
lauslim12 <31909304+lauslim12@users.noreply.github.com>
2022-09-22 14:12:34 +0000

    fix: use environment variables from the server instead of the client

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  serverRuntimeConfig: {
+    API_KEY: process.env.TINYURL_API_KEY,
+  },
   reactStrictMode: false,
   swcMinify: true,
 };
