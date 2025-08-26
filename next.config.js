5da96735b097052d545806f862ade1c4e666c4fb
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-08-26 20:17:09 +0700

    feat: update server runtime config with Cloudflare credentials

--- next.config.js
+++ next.config.js
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
 	serverRuntimeConfig: {
-		API_KEY: process.env.TINYURL_API_KEY,
+		CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID,
+		CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
 	},
 	reactStrictMode: true,
 };
