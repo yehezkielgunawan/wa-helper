0220c8e0a54feef30ef8599adfc2bdb7bbe652e8
yehezkielgunawan <yehezkiel.gunawan@binus.ac.id>
2025-12-12 17:56:44 +0700

    Upgrade Next.js to 16 and bump dependencies
    Remove serverRuntimeConfig from next.config.js. Add import for .next
    routes types in next-env.d.ts. Update tsconfig (jsx -> react-jsx,
    include .next/dev/types). Upgrade React, ReactDOM and various
    dev/devtools and regenerate pnpm lockfile

--- next.config.js
+++ next.config.js
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
-	serverRuntimeConfig: {
-		CLOUDFLARE_ACCOUNT_ID: process.env.CLOUDFLARE_ACCOUNT_ID,
-		CLOUDFLARE_API_TOKEN: process.env.CLOUDFLARE_API_TOKEN,
-	},
 	reactStrictMode: true,
 };
